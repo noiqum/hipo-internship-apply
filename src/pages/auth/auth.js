998188616ca3951049815913d5d76491a3c44d52
unknown <onurcoskun99@gmail.com>
2020-10-19 16:31:21 +0300

    step -1 and auth pages transition animation added

--- src/pages/auth/auth.js
+++ src/pages/auth/auth.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { checkDate } from '../../utils/utils';
 import Form from '../../components/form/form';
+import { motion } from 'framer-motion';
 function Auth() {
 
     const [date, setDate] = useState(null);
@@ -11,9 +12,14 @@ function Auth() {
     }, [])
 
     return (
-        <div className='auth'>
+        <motion.div
+            className='auth'
+            exit={{ x: '-100%', transition: { duration: 2, ease: 'easeOut' } }}
+            animate={{ x: 0, transition: { duration: 2, ease: 'easeOut' } }}
+            initial={{ x: '100%' }}
+        >
             {date ? <Form /> : <p>Application Process ended 30/10/2020 ,follow us ,we will announce next sessions</p>}
-        </div>
+        </motion.div>
     )
 }
 
--- src/pages/step1/step-1.js
+++ src/pages/step1/step-1.js
@@ -1,10 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { motion } from 'framer-motion'
 
 function Step1() {
+
+
+
     return (
-        <div>
+        <motion.div
+            exit={{ x: 100, opacity: 0, transition: { duration: 2, ease: 'easeOut' } }}
+            animate={{ x: 0, transition: { duration: 2, ease: 'easeOut' } }}
+            initial={{ x: '100%' }}
+            className='step1'
+        >
             <p>step 1</p>
-        </div>
+        </motion.div>
     )
 }
 
