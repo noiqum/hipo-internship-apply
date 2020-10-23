83f00eb449836a10ca261f77eaf9405309e5f560
unknown <onurcoskun99@gmail.com>
2020-10-23 12:05:10 +0300

    error page done

--- src/pages/error/down.js
+++ src/pages/error/down.js
@@ -4,17 +4,17 @@ import { useLocation } from 'react-router-dom'
 function Down() {
 
     const location = useLocation()
-    // const fields = location.state.fields;
-    // const msgs = location.state.msgs
+    const fields = location.state.fields;
+    const msgs = location.state.msgs
     return (
         <div className='down'>
             <p>Opps There is an Error</p>
-            {/* {fields.map((elm) => {
+            {fields.map((elm) => {
                 return <p key={fields[elm]}> field:{elm}</p>
             })}
             {msgs.map((elm) => {
                 return <p>Message: {elm}</p>
-            })} */}
+            })}
             <a href="/">HomePage</a>
         </div>
     )
